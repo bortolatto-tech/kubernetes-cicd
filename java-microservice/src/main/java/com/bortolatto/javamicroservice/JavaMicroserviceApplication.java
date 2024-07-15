efa155671e16230d15e1ab75d2db50c4faae35a8
Eduardo Bortolatto <eduardo.bortolatto@softplan.com.br>
2024-07-15 00:04:03 -0300

    :rocket: microserviço base para testar ci/cd

---
+++ java-microservice/src/main/java/com/bortolatto/javamicroservice/JavaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.bortolatto.javamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMicroserviceApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(JavaMicroserviceApplication.class, args);
+   }
+
+}
